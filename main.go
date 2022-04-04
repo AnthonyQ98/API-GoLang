155ccba07cac01cba640e26fae3c0c3d7ae06b9a
anthonyq98 <antoqdublin@gmail.com>
2022-04-04 12:11:20 +0100

    Get Phone by ID API endpoint

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-	//"errors"
+	"errors"
 )
 
 type phone struct{
@@ -18,6 +18,28 @@ var phones = []phone{
 	{ID: "2", Model: "iPhone 6", Year: "2014", Quantity: 9},
 	{ID: "3", Model: "iPhone X", Year: "2017", Quantity: 2},
 }
+
+func phoneById(c *gin.Context) {
+	id := c.Param("id")
+	phone, err := getPhoneById(id)
+
+	if err != nil {
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, phone)
+}
+
+func getPhoneById(id string) (*phone, error) {
+	for i, p := range phones {
+		if p.ID == id {
+			return &phones[i], nil
+		}
+	}
+
+	return nil, errors.New("Phone not found.")
+}
+
 
 func getPhones(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, phones)
@@ -37,6 +59,7 @@ func createPhone(c *gin.Context) {
 func main(){
 	router := gin.Default()
 	router.GET("/phones", getPhones)
+	router.GET("/phones/:id", phoneById)
 	router.POST("/phones", createPhone)
 	router.Run("localhost:8080")
 }

562fbfda93192a3135c8bcf0221091afc80318cd
anthonyq98 <antoqdublin@gmail.com>
2022-04-04 11:40:54 +0100

    Setting up struct of phone models

--- main.go
+++ main.go
@@ -5,3 +5,20 @@ import (
 	"github.com/gin-gonic/gin"
 	"errors"
 )
+
+type phone struct{
+	ID       string `json:"id"`
+	Model    string `json:"model"`
+	Year     string `json:"year"`
+	Quantity int    `json:"quantity"`
+}
+
+var phones = []phone{
+	{ID: "1", Model: "iPhone 11", Year: "2019", Quantity: 4},
+	{ID: "2", Model: "iPhone 6", Year: "2014", Quantity: 9},
+	{ID: "3", Model: "iPhone X", Year: "2017", Quantity: 2},
+}
+
+func main(){
+	
+}
